4142c8863df64944f59721e4c33745d82a30a867
christos michail <christos.m.michael@gmail.com>
2020-05-21 23:35:05 +0300

    intergrate with profile manager

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -1,9 +1,11 @@
-from flask import jsonify
+from flask import jsonify, request
 from FlaskApp import app
 import random
 import csv
 import json
+import requests
 
+PROFILE_MANAGER_API = 'https://wenet.u-hopper.com/dev/profile_manager'
 
 class User(object):
     def __init__(self, user_id, name):
@@ -28,19 +30,18 @@ class Task(object):
 
 def import_users():
     users = []
-    with open('FlaskApp/data/users.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            users.append(User(user_id=row[0], name=row[1]))
+    try:
+        for i in range(1, 5):
+            r = requests.get(PROFILE_MANAGER_API+'/profiles/' + str(i))
+            users.append(User(user_id=r.json()['id'], name=r.json()['name']))
+    except Exception as e:
+        return 'Did not get user!'+ e
     return users
 
 
 def import_tasks():
     tasks = []
-    with open('FlaskApp/data/tasks.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            tasks.append(Task(task_id=row[0], user_id='', description=row[1]))
+    tasks.append(Task(task_id='5eb90acc7b39534433cc00e9', user_id='', description='Lets Have dinner'))
     return tasks
 
 
@@ -49,25 +50,14 @@ def build_relation_data():
     social_relations = []
     relation_types = ['family', 'friend', 'contact', 'colleague', 'other']
     sources = ['Facebook', 'LinkedIn', 'Instagram']
-    for i in range(random.randint(300, 500)):
+    for i in range(random.randint(50, 80)):
         weight = round(random.uniform(0.0, 1.0), 1)
         user, friend = random.choice(users), random.choice(users)
-        relation_type, source_id = random.choice(relation_types),random.choice(sources)
-        if user != friend:
+        type, source_id = random.choice(relation_types),random.choice(sources)
+        if user.user_id != friend.user_id:
             social_relations.append(SocialRelation(user.user_id, friend.user_id,
-                                                   relation_type, weight, source_id))
+                                                   type, weight, source_id))
     return social_relations
-
-
-def match_user_for_task_classifier():
-    tasks = import_tasks()
-    matched_users_for_task = {}
-    with open('FlaskApp/data/rules.txt') as json_file:
-        rules = json.load(json_file)
-    for task in tasks:
-        matched_users_for_task[task.task_id] = {'description': 'Social Explanation',
-                                                'Summary': random.choice(rules)}
-    return matched_users_for_task
 
 
 def rank_users_for_task_classifier():
@@ -78,17 +68,16 @@ def rank_users_for_task_classifier():
         ranked_users_for_task[task.task_id] = {'description': 'Ordered list of User IDs',
                                                'schema': {'type': 'array',
                                                           'items': [user.user_id for user in
-                                                                    random.sample(users, random.randint(2, 20))]}}
+                                                                    random.sample(users, random.randint(2, 4))]}}
     return ranked_users_for_task
 
 
 social_relations = build_relation_data()
-matched_users_for_task = match_user_for_task_classifier()
 ranked_users_for_task = rank_users_for_task_classifier()
 
 
 @app.route("/")
-def hello():
+def home():
     return 'Wenet Home'
 
 
@@ -103,6 +92,8 @@ def show_social_relations(user_id):
                                            }
                                  }
         if user_social_relations[user_id]['schema']['items']:
+            r = requests.put(PROFILE_MANAGER_API+'/profiles/' + str(user_id),
+                             json={'relationships': [{'userId': '1', 'type': 'friend'}]})
             return jsonify(user_social_relations[user_id])
         else:
             return 'Not found!'
@@ -112,24 +103,26 @@ def show_social_relations(user_id):
 
 @app.route("/social/explanations/<user_id>/<task_id>/", methods=['GET'])
 def show_social_explanations(user_id, task_id):
-    try:
-        if matched_users_for_task[task_id]:
-            return jsonify(matched_users_for_task[task_id])
-        else:
-            return 'Not found!'
-    except Exception as e:
-        return 'Not found!'
+    explanation = {"Summary":{"Explanation":{"Context":["relevant(development)"],"Facts":["worksOn(development, bob"],
+                                             "Triggered rules":{"R1":"worksOn(X,Y) AND relevant(X)IMPLIES suggest(Y)"}}},
+                   "description":"Social Explanation"}
+    return jsonify(explanation)
 
 
-@app.route("/social/preferences/<user_id>/<task_id>/", methods=['GET'])
+@app.route("/social/preferences/<user_id>/<task_id>/", methods=['GET','POST'])
 def show_social_preferences(user_id, task_id):
-    try:
-        if ranked_users_for_task[task_id]:
-            return jsonify(ranked_users_for_task[task_id])
-        else:
+    task_id ='5eb90acc7b39534433cc00e9'
+    if request.method == "POST":
+        data = request.json
+        return jsonify(data)
+    else:
+        try:
+            if ranked_users_for_task[task_id]:
+                return jsonify(ranked_users_for_task[task_id])
+            else:
+                return 'Not found!'
+        except Exception as e:
             return 'Not found!'
-    except Exception as e:
-        return 'Not found!'
 
 
 if __name__ == "__main__":
