6a553d899cd5859435c3ca3c201617feeb7c9be0
christos michail <christos.m.michael@gmail.com>
2021-04-16 20:29:18 +0300

    Update views.py

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -16,7 +16,7 @@ COMP_AUTH_KEY = 'zJ9fwKb1CzeJT7zik_2VYpIBc_yclwX4Vd7_lO9sDlo'
 
 @app.route("/")
 def home():
-    return 'Wenet Home V3.05'
+    return 'Wenet Home V3.06'
 
 
 @app.route("/social/profile/streambase", methods=['POST'])
