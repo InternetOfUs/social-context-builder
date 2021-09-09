872bf49fd5969951e47158ed85a1db391cb60c4c
christos michail <christos.m.michael@gmail.com>
2021-09-09 17:53:11 +0300

    Update __init__.py

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
+app.config['CELERY_BROKER_URL'] = 'redis://redis:6379'
 db = SQLAlchemy(app)
 from FlaskApp import views
 db.create_all()
