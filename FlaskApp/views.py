d9e8328d655e3330d866c786d624503cec62ac94
christos michail <christos.m.michael@gmail.com>
2021-09-16 16:18:54 +0300

    Update views.py

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -179,9 +179,10 @@ def show_social_preferences_selection(user_id, task_id, selection):
         for answer in request.json['data']:
             data['users_IDs'].append(answer['userId'])
         suggested_entities = get_profiles_from_profile_manager(data)
-    user_preference = suggested_entities[selection] #dummy, as for now
+    user_preference = suggested_entities[int(selection)] #dummy, as for now
     model = ranking_model(user_preference, suggested_entities)
     print(model)
+    return model
 
 def rank_profiles(user_ids):
     MODEL = [0.5] * 5
