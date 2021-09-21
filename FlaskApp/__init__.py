4039e695e394b17aad07d3fb0fe9f2373b037b7d
christos michail <christos.m.michael@gmail.com>
2021-09-21 14:40:26 +0300

    Update __init__.py

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -4,9 +4,9 @@ import logging
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
 app.config['CELERY_BROKER_URL'] = 'redis://redis:6379'
-logging.basicConfig(filename='social_context_builder.log', level=logging.INFO, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+logging.basicConfig(filename='social_context_builder.log', level=logging.INFO, format=f'%(asctime)s Social Context Builder %(levelname)s : %(message)s')
 db = SQLAlchemy(app)
 from FlaskApp import views
-db.drop_all()
+#db.drop_all()
 db.create_all()
 
