e8a06061071eb557b2725951c77589916e547593
christos michail <christos.m.michael@gmail.com>
2021-10-07 19:24:44 +0300

    Update views.py

--- FlaskApp/views.py
+++ FlaskApp/views.py
@@ -185,7 +185,6 @@ def ranking_all(user_id):
 @app.route("/social/notification/interaction", methods=['POST'])
 def social_notification_interaction():
     try:
-        app.logger.info('received interaction')
         data = request.json
         async_social_ties_learning.delay(data)
         return{}
