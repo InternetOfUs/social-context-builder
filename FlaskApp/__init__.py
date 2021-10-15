977ad08668e8e5586da3857bcca1aabce8fb119b
christos michail <christos.m.michael@gmail.com>
2021-10-15 14:25:53 +0300

    adding fix for celery url

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import logging
+import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/database.db'
-app.config['CELERY_BROKER_URL'] = 'redis://redis:6379'
+app.config['CELERY_BROKER_URL'] = os.environ['CELERY_BROKER_URL']
 logging.basicConfig(filename='FlaskApp/logs/social_context_builder.log', level=logging.INFO, format=f'%(asctime)s Social Context Builder %(levelname)s : %(message)s')
 db = SQLAlchemy(app)
 from FlaskApp import views
